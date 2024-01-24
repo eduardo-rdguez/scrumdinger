611dee67759d996da76e3b9091caf6218f644a08
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2024-01-24 17:28:22 -0600

    creates a new project using the ios app template

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Eduardo Peralta Rodríguez on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
