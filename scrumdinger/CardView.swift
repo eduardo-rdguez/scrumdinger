15d69d9e77ba570fdce86ced5c4c268c3373365c
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2024-01-25 14:05:58 -0600

    create and stylize the card view to summarize the daily scrum

---
+++ scrumdinger/CardView.swift
@@ -0,0 +1,36 @@
+//
+//  CardView.swift
+//  scrumdinger
+//
+//  Created by Eduardo Peralta Rodríguez on 25/01/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lentgthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
