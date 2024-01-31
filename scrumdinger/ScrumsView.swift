b2ab79452a289eccbb0d5c07dc7970acc4a1bc66
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2024-01-31 16:45:58 -0600

    create the scrums view to display a list of daily scrums

---
+++ scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  View.swift
+//  scrumdinger
+//
+//  Created by Eduardo Peralta Rodríguez on 31/01/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
