6c7f4a7cdb49de7b874d93065bcc0de9192afe88
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2024-02-08 21:18:41 -0600

    add a theme view for display the components of a theme

---
+++ scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  scrumdinger
+//
+//  Created by Eduardo Peralta Rodríguez on 08/02/24.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
